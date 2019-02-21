8b5744bfebfbd39975d1004b152e6da641b687c6
albenarammou <albenarammou@gmail.com>
2019-02-21 14:01:09 +0200

    http

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { WeightEntriesService } from '../weight-entries.service';
+import { Entry } from '../model/entry';
+
+@Component({
+  selector: 'hm-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  showBodyFat = true;
+  entries: Entry[];
+
+  constructor(public entriesSvc: WeightEntriesService) { }
+
+  ngOnInit() {
+    this.entriesSvc.getEntries().subscribe(entries => {
+      this.entries = entries;
+    })
+  }
+
+  toggleBodyFat(){ 
+    this.showBodyFat=!this.showBodyFat;
+  }
+
+  createNewEntry(entry: Entry){
+    this.entriesSvc.addEntry(entry)
+  }
+}
